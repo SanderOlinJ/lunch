75cfe468ba351186089c08a169ae6c418f61817c
Sander Johansen <sander.olin@gmail.com>
2024-08-05 22:53:03 +0200

    Continued doing God's work.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,31 @@
+'use client';
 import LunchTime from "@/components/LunchTime";
 import NotLunchTime from "@/components/NotLunchTime";
-import {ReactElement} from "react";
+import {ReactElement, useEffect, useState} from "react";
 
 export default function Home(): ReactElement {
-    const currentTime: Date = new Date()
+    const [isLunchTime, setIsLunchTime] = useState<boolean>(false);
+
+    useEffect(() => {
+        const checkTime = () => {
+            const currentTime: Date = new Date();
+            const isLunch = currentTime.getHours() >= 11 && currentTime.getHours() <= 12;
+            setIsLunchTime(isLunch);
+        };
+
+        // Check time immediately on mount
+        checkTime();
+
+        // Set an interval to check time every minute
+        const intervalId = setInterval(checkTime, 1000);
+
+        // Clean up interval on component unmount
+        return () => clearInterval(intervalId);
+    }, []);
 
     return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        { currentTime.getHours() >= 11 && currentTime.getHours() <= 12 ?
+        { isLunchTime ?
             <LunchTime/> :
             <NotLunchTime/>
         }
