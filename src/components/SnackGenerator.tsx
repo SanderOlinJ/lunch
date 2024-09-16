328ac0591862f446143908d00b9c8c9a97dd17c9
Sander Johansen <sander.olin@gmail.com>
2024-09-16 14:03:20 +0200

    Fixed some issues with the SnackGenerator.tsx

--- src/components/SnackGenerator.tsx
+++ src/components/SnackGenerator.tsx
@@ -6,14 +6,14 @@ interface Snack {
 }
 
 const snackList: Snack[] = [
-    { name: 'YT proteinpudding', fileName: 'proteinpudding.png' },
-    { name: 'YT proteinshake', fileName: 'proteinshake.png' },
-    { name: 'Cottage cheese vanilje', fileName: 'cottagecheese.png' },
-    { name: 'Gresk yoghurt', fileName: 'greskyoghurt.png' },
-    { name: 'Rød smoothie', fileName: 'rodsmoothie.png' },
-    { name: 'Gul smoothie', fileName: 'gulsmoothie.png' },
-    { name: 'Go morgen morell', fileName: 'gomorgen_morell.png' },
-    { name: 'Go morgen mango', fileName: 'gomorgen_mango.png' },
+    { name: 'YT Proteinpudding', fileName: 'proteinpudding.png' },
+    { name: 'YT Proteinshake', fileName: 'proteinshake.png' },
+    { name: 'Cottage Cheese Vanilje', fileName: 'cottagecheese.png' },
+    { name: 'Gresk Yoghurt', fileName: 'greskyoghurt.png' },
+    { name: 'Rød Smoothie', fileName: 'rodsmoothie.png' },
+    { name: 'Gul Smoothie', fileName: 'gulsmoothie.png' },
+    { name: 'Go Morgen Morell', fileName: 'gomorgen_morell.png' },
+    { name: 'Go Morgen Mango', fileName: 'gomorgen_mango.png' },
 ];
 
 const SnackGenerator: React.FC = () => {
@@ -33,38 +33,30 @@ const SnackGenerator: React.FC = () => {
 
     return (
         <div style={{position: "absolute", bottom: "25px", zIndex: 30}}>
-            <p className="text-center text-xl font-bold">Trenger du motivasjon, siden det ikke er lunsj?</p>
+            <p className="text-center text-lg font-bold">Trenger du motivasjon, siden det ikke er lunsj?</p>
             <p className="text-center text-lg">Nyt en snack!</p>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4" onClick={generateRandomSnack}>Tilfeldig snack</button>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
+                    onClick={generateRandomSnack}>
+                Tilfeldig snack
+            </button>
             {modalOpen && randomSnack && (
-                <div
-                    style={{
-                        width: "100vw",
-                        height: "100vh",
-                        position: "fixed", 
-                        top: 0,
-                        left: 0,
-                        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        zIndex: 50
-                    }}
+                <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50"
                     onClick={closeModal}
                 >
                     <div
-                        className="p-4 rounded shadow-lg"
-                        style={{
-                            backgroundColor: 'rgb(66, 16, 66)',
-                            borderRadius: '10px',
-                            zIndex: 60
-                        }}
-                        onClick={(e) => e.stopPropagation()} 
+                        className="relative bg-white dark:bg-custom-dark-modal px-20 py-10 rounded-lg shadow-lg z-60"
+                        onClick={(e) => e.stopPropagation()}
                     >
+                        <button
+                            className="absolute -top-2.5 -left-2.5 bg-white border-2 border-purple-900 rounded-full
+                            w-8 h-8 flex justify-center items-center cursor-pointer text-purple-900 text-2xl"
+                            onClick={closeModal}
+                        >
+                            &times;
+                        </button>
                         <p className="text-center text-lg">Gå og ta deg en</p>
                         <h2 className="text-center text-2xl font-bold mb-4">{randomSnack?.name}</h2>
-                        <img src={`/${randomSnack?.fileName}`} alt={randomSnack?.name} style={{ height: '150px', margin: "auto" }} />
-                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4" onClick={closeModal}>Lukk</button>
+                        <img className="h-36 mx-auto" src={`/${randomSnack?.fileName}`} alt={randomSnack?.name}/>
                     </div>
                 </div>
             )}
